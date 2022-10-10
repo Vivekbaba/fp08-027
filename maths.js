296bb002497561fdce9a731e887ca7537b0cb7ac
vivek singh <viveksingh2472@gmail.com>
2022-10-10 21:10:18 +0530

    maths and variable

---
+++ maths.js
@@ -0,0 +1,7 @@
+let x=3;
+let y=2;
+
+console.log("sum is",x+y);
+console.log("sub is",x-y);
+console.log("mult is",x*y);
+console.log("div is",x/y);
---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = "Vivek Kumar Singh";
+let school="Guru Nanak Public School";
+let grade="A+";
+let section="A";
+let roll_no="69";
+let maths="84";
+let english="86";
+let science="85";
+console.log(name,school,grade,section,roll_no,maths,english,science);
